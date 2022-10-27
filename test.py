4932df957b54231f05a68f1296d7643197e5f17a
VictorFernandezF <victorfernandezflores2@gmail.com>
2022-10-27 15:11:00 +0200

    feat: add the rest of the letters

--- test.py
+++ test.py
@@ -7,59 +7,65 @@ from tkinter import ttk
 morse = dict(
     a = '. -',
     b = '— · · ·',
-    c = '',
-    d = '',
-    e = '',
-    f = '',
-    g = '',
-    h = '',
-    i = '',
-    j = '',
-    k = '',
-    l = '',
-    m = '',
-    n = '',
-    o = '- - -',
-    p = '',
-    q = '',
-    r = '',
-    s = '. . .',
-    t = '',
-    u = '',
-    v = '',
-    w = '',
-    x = '',
-    y = '',
-    z = ''
+    c = '— · — ·',
+    ch = '— — — —',
+    d = '— · ·',
+    e = '.',
+    f = '· · — ·',
+    g = '— — ·',
+    h = '· · · ·',
+    i = '. .',
+    j = '· — — —',
+    k = '— · —',
+    l = '· — · ·',
+    m = '— —',
+    n = '— ·',
+    ñ = '— — · — —',
+    o = '— — —',
+    p = '. — — .',
+    q = '— — · —',
+    r = '· — ·',
+    s = '·  · ·',
+    t = '—',
+    u = '· · —',
+    v = '· · · —',
+    w = '· — —',
+    x = '— · · —',
+    y = '— · — —',
+    z = '— — · ·'
     )
 #Create an instance of Tkinter frame
 win= Tk()
 
 #Set the geometry of Tkinter frame
-win.geometry("750x250")
+win.geometry("750x150")
 
 def display_text():
     global entry
     string= entry.get()
     res = ""
+    check = 0
     for i in string:
-        if i == " ":
+        if i == ' ':
+            res = "Solo una palabra por favor"
+            check = 1
+        if check == 0:
+            res = res + morse[i]
             res = res + "  "
-        res = res + morse[i]
-        res = res + "  "
     label.configure(text=res)
 
-
-#Initialize a Label to display the User Input
-label=Label(win, text="", font=("Courier 22 bold"))
-label.config(font=('Helvatical bold',20))
-label.pack()
+#Label info
+info=Label(win, text="Introduce texto para convertirlo en morse", font=("Courier 18 bold"))
+info.pack()
 
 #Create an Entry widget to accept User Input
 entry= Entry(win, width= 40)
 entry.focus_set()
 entry.pack()
 
+#Initialize a Label to display the User Input
+label=Label(win, text="", font=("Courier 17"))
+label.pack()
 #Create a Button to validate Entry Widget
 ttk.Button(win, text= "Okay",width= 20, command= display_text).pack(pady=20)
 
